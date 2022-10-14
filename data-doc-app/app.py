5c0c548c66f354da5cc5c16fd853bbf786e04825
Maiterivas <maiterivas09@gmail.com>
2022-10-14 12:11:01 -0500

    fixing html file paths, index and sample inner

--- data-doc-app/app.py
+++ data-doc-app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-from new_data_doc_main import prediction 
-
+from new_data_doc_main import prediction, data, clf
+import pickle
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/prediction_model")
+def prediction_model():
+    return render_template("sample-inner-page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
