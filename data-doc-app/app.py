4a8b27c421b2d3d3d67bde49909c001760a9defd
Sydney Cohen <103545183+SydCo99@users.noreply.github.com>
2022-10-14 17:22:20 -0500

    added form input to talk to flask

--- data-doc-app/app.py
+++ data-doc-app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, redirect, request
 from flask_pymongo import PyMongo
-#from new_data_doc_main import prediction, forest
+from new_data_doc_main import prediction, forest
 import pickle
+
 
 app = Flask(__name__)
 
@@ -14,10 +15,12 @@ def index():
 @app.route("/prediction_model", methods=['POST','GET'])
 def prediction_model():
 
-    symptoms = request.form.getlist('#ID name')
-    prediction = model.prediction(symptoms)
+    symptoms = request.args['symptom']
+    print(symptoms)
+    #prediction = model.prediction(symptoms)
     pred_output = prediction(symptoms)
-    return render_template("sample-inner-page.html", pred_output=pred_output, prediction=prediction, symptoms=symptoms)
+
+    return render_template("sample-inner-page.html", disease = pred_output)
 
 if __name__ == "__main__":
     app.run(debug=True)
