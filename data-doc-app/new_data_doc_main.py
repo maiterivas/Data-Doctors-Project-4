d4c7f82df8f3a329afe17a8203290b883f62dc66
Maiterivas <maiterivas09@gmail.com>
2022-10-15 14:17:37 -0500

    updating model file path

--- data-doc-app/new_data_doc_main.py
+++ data-doc-app/new_data_doc_main.py
@@ -9,6 +9,7 @@ Original file is located at
 
 # Commented out IPython magic to ensure Python compatibility.
 import pandas as pd
+import os
 # %matplotlib inline
 from matplotlib import pyplot as plt
 from sklearn.datasets import make_classification
@@ -26,7 +27,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 #load in the data 
-file = "data-doc-app/static/resources/one_hot_data.csv"
+file = "static/resources/one_hot_data.csv"
 df = pd.read_csv(file)
 df.head()
 
