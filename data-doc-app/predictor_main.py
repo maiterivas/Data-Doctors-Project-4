122764d4a3a6887971457c0bb83cd9ce1a0aadce
Maiterivas <maiterivas09@gmail.com>
2022-10-16 18:46:18 -0500

    fixing index.html

--- data-doc-app/predictor_main.py
+++ data-doc-app/predictor_main.py
@@ -27,6 +27,10 @@ warnings.filterwarnings("ignore")
 
 #load in the data 
 file = "data-doc-app/static/resources/one_hot_data.csv"
+
+#maite needs this line for app.py to run
+#file = "static/resources/one_hot_data.csv"
+
 df = pd.read_csv(file)
 df.head()
 
